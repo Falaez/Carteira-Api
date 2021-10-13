e8650622eb313229ffe61741d5f691845ddcf35f
Mário Henrique <mariohenriquelp@gmail.com>
2021-10-13 17:37:13 -0300

    paginacao

--- src/main/java/br/com/alura/carteira/controller/TransacaoController.java
+++ src/main/java/br/com/alura/carteira/controller/TransacaoController.java
@@ -1,12 +1,11 @@
 package br.com.alura.carteira.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-
 import javax.validation.Valid;
 
-import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,24 +14,23 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.alura.carteira.dto.TransacaoDto;
 import br.com.alura.carteira.dto.TransacaoFormDto;
-import br.com.alura.carteira.modelo.Transacao;
+import br.com.alura.carteira.service.TransacaoService;
 
 @RestController
 @RequestMapping("/transacoes")
 public class TransacaoController {
 	
-	private List<Transacao> transacoes = new ArrayList<>();
-	private ModelMapper modelMapper = new ModelMapper();
+	@Autowired
+	private TransacaoService service;
+	
 	
 	@GetMapping
-	public List<TransacaoDto> Listar() {
-		return transacoes.stream().map(t -> modelMapper.map(t, TransacaoDto.class)).collect(Collectors.toList());
+	public Page<TransacaoDto> Listar(@PageableDefault(size = 10) Pageable paginacao) {
+		return service.listar(paginacao);
 	}
 	
 	@PostMapping
 	public void cadastrar(@RequestBody @Valid TransacaoFormDto dto) {
-		Transacao transacao = modelMapper.map(dto, Transacao.class);
-		
-		transacoes.add(transacao);
+		service.cadastrar(dto);
 	}
 }
--- src/main/java/br/com/alura/carteira/service/TransacaoService.java
+++ src/main/java/br/com/alura/carteira/service/TransacaoService.java
@@ -1,13 +1,12 @@
 package br.com.alura.carteira.service;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import br.com.alura.carteira.dto.TransacaoDto;
@@ -22,9 +21,9 @@ public class TransacaoService {
 	private TransacaoRepository transacaoRespository;
 	private ModelMapper modelMapper = new ModelMapper();
 	
-	public List<TransacaoDto> listar(){
-		List<Transacao> transacao = transacaoRespository.findAll();
-		return transacao.stream().map(t -> modelMapper.map(t, TransacaoDto.class)).collect(Collectors.toList());
+	public Page<TransacaoDto> listar(Pageable paginacao){
+		Page<Transacao> transacao = transacaoRespository.findAll(paginacao);
+		return transacao.map(t -> modelMapper.map(t, TransacaoDto.class));
 	}
 	
 	@Transactional
