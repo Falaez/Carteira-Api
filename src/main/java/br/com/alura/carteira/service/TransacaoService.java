fefc2a42595aa6de87b58d9d61ad37be5ac7ab23
Mário Henrique <mariohenriquelp@gmail.com>
2021-11-01 13:50:49 -0300

    testes da classe transacao service

--- src/main/java/br/com/alura/carteira/service/TransacaoService.java
+++ src/main/java/br/com/alura/carteira/service/TransacaoService.java
@@ -1,5 +1,6 @@
 package br.com.alura.carteira.service;
 
+import javax.persistence.EntityNotFoundException;
 import javax.transaction.Transactional;
 import javax.validation.Valid;
 
@@ -12,13 +13,19 @@ import org.springframework.stereotype.Service;
 import br.com.alura.carteira.dto.TransacaoDto;
 import br.com.alura.carteira.dto.TransacaoFormDto;
 import br.com.alura.carteira.modelo.Transacao;
+import br.com.alura.carteira.modelo.Usuario;
 import br.com.alura.carteira.repository.TransacaoRepository;
+import br.com.alura.carteira.repository.UsuarioRepository;
 
 @Service
 public class TransacaoService {
 	
 	@Autowired
 	private TransacaoRepository transacaoRespository;
+	
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+	
 	private ModelMapper modelMapper = new ModelMapper();
 	
 	public Page<TransacaoDto> listar(Pageable paginacao){
@@ -28,10 +35,20 @@ public class TransacaoService {
 	
 	@Transactional
 	public TransacaoDto cadastrar(@Valid TransacaoFormDto dto) {
+	Long idUsuario = dto.getUsuarioId();
+	
+	try {
+	Usuario usuario = usuarioRepository.getById(idUsuario);
+	
 	Transacao transacao= modelMapper.map(dto, Transacao.class);
 	transacao.setId(null);
+	transacao.setUsuario(usuario);
 	
 	transacaoRespository.save(transacao);
 	return modelMapper.map(transacao, TransacaoDto.class);
+	}
+	catch (EntityNotFoundException e) {
+		throw new IllegalArgumentException("Usuario inexistente!");
+	}
 	}
 }
